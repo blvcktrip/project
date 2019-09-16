9c2784464459a0d1bfe55eb38cd396208a152c47
Vlad <example@gmail.com>
2019-09-16 12:19:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+let persone = {
+    name: "Jhon",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[2]);
+
+//alert("Hello World!");
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+
+// let answer = prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(null));
+
+// console.log("arr" + " - object");
+// console.log(4 + + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
